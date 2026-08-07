4813b9ea37218db54f8984e5d5fb8ab72c88e097
Mr.Schyzo <16671172+MrSchyzo@users.noreply.github.com>
2026-08-07 23:36:40 +0200

    Add file loader

---
+++ java/src/main/java/com/mrschyzo/hungarian/infrastructure/FileLotteryLoader.java
@@ -0,0 +1,29 @@
+package com.mrschyzo.hungarian.infrastructure;
+
+import com.mrschyzo.hungarian.domain.Lottery;
+import com.mrschyzo.hungarian.domain.PickParser;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileLotteryLoader {
+    private PickParser parser = new PickParser();
+
+    public FileLotteryLoader() {}
+
+    public Lottery load(Path path) {
+        var lottery = new Lottery();
+        try(var lines = Files.lines(path)) {
+            lines.forEach(x -> parseAndAccept(x, lottery));
+            return lottery;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void parseAndAccept(String rawPick, Lottery lottery) {
+        try {
+            lottery.acceptPick(parser.parse(rawPick));
+        } catch (Exception _) {}
+    }
+}
---
+++ java/src/test/java/com/mrschyzo/hungarian/infrastructure/FileLotteryLoaderTest.java
@@ -0,0 +1,46 @@
+package com.mrschyzo.hungarian.infrastructure;
+
+import com.mrschyzo.hungarian.domain.Lottery;
+import com.mrschyzo.hungarian.domain.Pick;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.util.Objects;
+
+public class FileLotteryLoaderTest {
+    @Test
+    public void parse_lottery_file_successfully() throws URISyntaxException {
+        FileLotteryLoader loader = new FileLotteryLoader();
+        Path path = resource("simple_few_lines.txt");
+        Lottery lottery = loader.load(path);
+        var result = lottery.getWinnersHistogram(Pick.of(10, 20, 30, 40, 50));
+        Assertions.assertEquals(2, result.get2MatchCount());
+        Assertions.assertEquals(1, result.get3MatchCount());
+        Assertions.assertEquals(1, result.get4MatchCount());
+        Assertions.assertEquals(1, result.get5MatchCount());
+    }
+
+    @Test
+    public void lottery_parsing_ignores_broken_lines() throws URISyntaxException {
+        FileLotteryLoader loader = new FileLotteryLoader();
+        Path path = resource("some_broken_lines.txt");
+        Lottery lottery = loader.load(path);
+        var result = lottery.getWinnersHistogram(Pick.of(10, 20, 30, 40, 50));
+        Assertions.assertEquals(2, result.get2MatchCount());
+        Assertions.assertEquals(1, result.get3MatchCount());
+        Assertions.assertEquals(1, result.get4MatchCount());
+        Assertions.assertEquals(1, result.get5MatchCount());
+    }
+
+    private Path resource(String name) throws URISyntaxException {
+        return Path.of(
+                Objects.requireNonNull(
+                        getClass()
+                                .getClassLoader()
+                                .getResource(name)
+                ).toURI()
+        );
+    }
+}
