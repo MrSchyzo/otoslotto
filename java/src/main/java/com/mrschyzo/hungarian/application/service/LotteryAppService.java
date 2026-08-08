bb80830dbdc21e49fc538a1b0b088588c04610a2
Mr.Schyzo <16671172+MrSchyzo@users.noreply.github.com>
2026-08-08 11:43:59 +0200

    Add query command

--- java/src/main/java/com/mrschyzo/hungarian/application/service/LotteryAppService.java
+++ java/src/main/java/com/mrschyzo/hungarian/application/service/LotteryAppService.java
@@ -1,18 +1,40 @@
 package com.mrschyzo.hungarian.application.service;
 
+import com.mrschyzo.hungarian.domain.Lottery;
 import com.mrschyzo.hungarian.domain.LotteryLoader;
+import com.mrschyzo.hungarian.domain.Pick;
+
+import java.util.Map;
 
 public class LotteryAppService {
     private LotteryLoader loader;
+    private Lottery lottery;
     public LotteryAppService(LotteryLoader loader) {
         this.loader = loader;
     }
 
     public void load() throws ApplicationException {
         try {
-            loader.load();
+            lottery = loader.load();
         } catch (Exception e) {
             throw new LotteryCannotBeLoadedException(e);
+        }
+    }
+
+    public Map<Integer, Integer> query(int[] pick) {
+        if (lottery == null || pick.length < 5) {
+            return Map.of(2,0,3,0,4,0,5,0);
+        }
+        try {
+            var winners = lottery.getWinnersHistogram(Pick.of(pick[0], pick[1], pick[2], pick[3], pick[4]));
+            return Map.of(
+                    2, winners.get2MatchCount(),
+                    3, winners.get3MatchCount(),
+                    4, winners.get4MatchCount(),
+                    5, winners.get5MatchCount()
+            );
+        } catch (Exception _) {
+            return Map.of(2,0,3,0,4,0,5,0);
         }
     }
 }
--- java/src/test/java/com/mrschyzo/hungarian/application/service/LotteryAppServiceTest.java
+++ java/src/test/java/com/mrschyzo/hungarian/application/service/LotteryAppServiceTest.java
@@ -2,8 +2,11 @@ package com.mrschyzo.hungarian.application.service;
 
 import com.mrschyzo.hungarian.domain.Lottery;
 import com.mrschyzo.hungarian.domain.LotteryLoader;
+import com.mrschyzo.hungarian.domain.Pick;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 public class LotteryAppServiceTest {
     @Test
@@ -19,5 +22,81 @@ public class LotteryAppServiceTest {
         };
         LotteryAppService service = new LotteryAppService(faulty);
         Assertions.assertThrows(LotteryCannotBeLoadedException.class, service::load);
+    }
+    @Test
+    public void query_service_command_returns_zeroed_histogram_if_it_was_not_loaded() {
+        LotteryLoader ok = Lottery::new;
+        LotteryAppService service = new LotteryAppService(ok);
+        Map<Integer, Integer> matches = service.query(new int[]{1,2,3,4,5});
+        var expected = Map.of(
+                2, 0,
+                3, 0,
+                4, 0,
+                5, 0
+        );
+        Assertions.assertEquals(expected, matches);
+    }
+    @Test
+    public void query_service_command_returns_zeroed_histogram_if_winning_ticket_has_not_5_elements() throws ApplicationException {
+        LotteryLoader ok = Lottery::new;
+        LotteryAppService service = new LotteryAppService(ok);
+        service.load();
+        Map<Integer, Integer> matches = service.query(new int[]{1,2,3,4});
+        var expected = Map.of(
+                2, 0,
+                3, 0,
+                4, 0,
+                5, 0
+        );
+        Assertions.assertEquals(expected, matches);
+    }
+    @Test
+    public void query_service_command_returns_zeroed_histogram_if_winning_ticket_has_duplicates() throws ApplicationException {
+        LotteryLoader ok = Lottery::new;
+        LotteryAppService service = new LotteryAppService(ok);
+        service.load();
+        Map<Integer, Integer> matches = service.query(new int[]{1,2,2,3,4});
+        var expected = Map.of(
+                2, 0,
+                3, 0,
+                4, 0,
+                5, 0
+        );
+        Assertions.assertEquals(expected, matches);
+    }
+    @Test
+    public void query_service_command_returns_zeroed_histogram_if_winning_ticket_has_invalid_values() throws ApplicationException {
+        LotteryLoader ok = Lottery::new;
+        LotteryAppService service = new LotteryAppService(ok);
+        service.load();
+        Map<Integer, Integer> matches = service.query(new int[]{0,1,2,3,4});
+        var expected = Map.of(
+                2, 0,
+                3, 0,
+                4, 0,
+                5, 0
+        );
+        Assertions.assertEquals(expected, matches);
+    }
+    @Test
+    public void query_service_command_returns_correct_histogram() throws ApplicationException {
+        LotteryLoader ok = () -> {
+            var lottery = new Lottery();
+            lottery.acceptPick(Pick.of(1,2,3,4,5));
+            lottery.acceptPick(Pick.of(2,1,3,4,5));
+            lottery.acceptPick(Pick.of(1,2,6,4,5));
+            lottery.acceptPick(Pick.of(7,3,6,4,5));
+            return lottery;
+        };
+        LotteryAppService service = new LotteryAppService(ok);
+        service.load();
+        Map<Integer, Integer> matches = service.query(new int[]{1,2,3,4,5});
+        var expected = Map.of(
+                2, 0,
+                3, 1,
+                4, 1,
+                5, 2
+        );
+        Assertions.assertEquals(expected, matches);
     }
 }
