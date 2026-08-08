a57ac0d309ba3a845da13ba4e18aa0d7479ec219
Mr.Schyzo <16671172+MrSchyzo@users.noreply.github.com>
2026-08-08 13:12:40 +0200

    Lottery benchmark

--- java/src/jmh/java/com/mrschyzo/hungarian/MyBenchmark.java
+++ java/src/jmh/java/com/mrschyzo/hungarian/MyBenchmark.java
@@ -1,11 +1,36 @@
 package com.mrschyzo.hungarian;
 
-import org.openjdk.jmh.annotations.Benchmark;
+import com.mrschyzo.hungarian.domain.Lottery;
+import com.mrschyzo.hungarian.domain.Pick;
+import org.openjdk.jmh.annotations.*;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.SampleTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
 public class MyBenchmark {
+    private Lottery lottery = new Lottery();
+    private Pick pick = Pick.of(1, 2, 3, 4, 5);
+    private Random random = new Random();
+
+    @Setup(Level.Trial)
+    public void setUp() {
+        var set = HashSet.<Integer>newHashSet(5);
+        for(int i = 0; i < 10_000_000; i++) {
+            while(set.size() < 5) {
+                set.add(random.nextInt(1, 91));
+            }
+            var rawPick = set.stream().mapToInt(x -> x).toArray();
+            lottery.acceptPick(Pick.of(rawPick[0], rawPick[1], rawPick[2], rawPick[3], rawPick[4]));
+            set.clear();
+        }
+    }
 
     @Benchmark
-    public int sum() {
-        return 1 + 2 ;
+    public Lottery.Histogram query() {
+        return lottery.getWinnersHistogram(pick);
     }
 }
